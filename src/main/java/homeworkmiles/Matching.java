c151c19b5542d8ad312d64be6bb4233237d4b974
Michal Ostrowski <dakkon@o2.pl>
2021-02-14 15:56:33 +0100

    0.3

---
+++ src/main/java/homeworkmiles/Matching.java
@@ -0,0 +1,18 @@
+package homeworkmiles;
+
+public class Matching {
+    static String miles = MilesToKm.readValue();
+
+    public static boolean matched(String miles) {
+        return miles.matches("^\\d+(\\.\\d+)?$");
+    }
+
+    public static double mil() {
+        return Double.parseDouble(miles);
+    }
+
+    public static double calc(double mil) {
+        double km = mil * 1.609344;
+        return km;
+    }
+}
